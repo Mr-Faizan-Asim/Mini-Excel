5486e73436038b991ea8f0ac7de4789dbc781a5b
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2023-11-09 22:10:16 +0500

    Final Mini Excel

--- ExcelApp/ExcelApp.cpp
+++ ExcelApp/ExcelApp.cpp
@@ -3,8 +3,10 @@
 #include<conio.h>
 #include<vector>
 #include <string>
-
+#include<fstream>
 using namespace std;
+// In order to manages FrontPAges
+
 
 class Cell {
 	int row;
@@ -59,8 +61,9 @@ public:
 	Node<T>* BottomLeft;
 	Node<T>* BottomRight;
 	Node<T>* Current;
+	vector<vector<T>>* data;
 public:
-//Constructor
+	//Constructor
 	Excell(T value)
 	{
 		TopLeft = nullptr;
@@ -70,22 +73,64 @@ public:
 		createIntial(5, 6, value);
 		Current = TopLeft;
 	}
-// CreateIntial 5 by 5
-	void createIntial(size_t row,size_t col,T value)
+	Excell(T value, string fileName)
+	{
+		TopLeft = nullptr;
+		TopRight = nullptr;
+		BottomLeft = nullptr;
+		BottomRight = nullptr;
+		createIntial(RowCal(fileName), colCal(fileName), value);
+		Current = TopLeft;
+	}
+	// fetcher
+
+	// Rows Calculator
+	int RowCal(string fileName)
+	{
+		fstream file;
+		file.open(fileName + ".txt", ios::in);
+		int row = 0;
+		while (!file.eof())
+		{
+			string d;
+			file >> d;
+			if (d != "")
+				row++;
+		}
+		file.close();
+		return row;
+	}
+	int colCal(string fileName)
+	{
+		fstream file;
+		file.open(fileName + ".txt", ios::in);
+		int col = 0;
+		string d;
+		file >> d;
+		for (int i = 0; i < d.size(); i++)
+		{
+			if (d[i] == ',')
+				col++;
+		}
+		file.close();
+		return col;
+	}
+	// CreateIntial 5 by 5
+	void createIntial(size_t row, size_t col, T value)
 	{
 		Node<T>* TempUp = nullptr;
-		for (int i = 0; i < col; i++)
+		for (int i = 0; i < row; i++)
 		{
-			for (int j = 0; j < row; j++)
+			for (int j = 0; j < col; j++)
 			{
 				Node<T>* next = new Node<T>(value);
 				if (TopLeft == nullptr || BottomLeft == nullptr)
 				{
 					next->up = TempUp;
-					if(TempUp != nullptr)
+					if (TempUp != nullptr)
 						TempUp->down = next;
 					next->right = TopLeft;
-					if(TopLeft == nullptr)
+					if (TopLeft == nullptr)
 						TopLeft = next;
 					BottomRight = next;
 					BottomLeft = next;
@@ -104,22 +149,20 @@ public:
 								BottomRight->left->up->right->down = next;
 								next->up = BottomRight->left->up->right;
 							}
-
-					
 				}
 			}
 			TempUp = BottomLeft;
 			BottomLeft = BottomLeft->down;
 			BottomRight = BottomLeft;
-			
+
 		}
 
 	}
-// Add A Row Above
+	// Add A Row Above
 	void addRowAbove(T val, Node<T>* Current)
 	{
 		Node<T>* temp = Current;
-		temp = TillNull(temp,lef);
+		temp = TillNull(temp, lef);
 		if (Current->up == nullptr)
 		{
 			bool checker = false;
@@ -133,11 +176,11 @@ public:
 					TopRight = next;
 					temp->up = next;
 					checker = true;
-					
+
 				}
 				else
 				{
-				
+
 					next->down = temp;
 					temp->up = next;
 					TopRight->right = next;
@@ -177,7 +220,7 @@ public:
 			}
 		}
 	}
-// Add Row Down
+	// Add Row Down
 	void addRowDown(T val, Node<T>* Current)
 	{
 		Node<T>* temp = Current;
@@ -239,8 +282,8 @@ public:
 			}
 		}
 	}
-// Add Columns Right
-	void addColumnRight(T val,Node<T>* Current)
+	// Add Columns Right
+	void addColumnRight(T val, Node<T>* Current)
 	{
 		Node<T>* temp = Current;
 		temp = TillNull(temp, up);
@@ -299,8 +342,8 @@ public:
 			}
 		}
 	}
-// Add Columns Right
-	void addColumnLeft(T val,Node<T>* Current)
+	// Add Columns Right
+	void addColumnLeft(T val, Node<T>* Current)
 	{
 		Node<T>* temp = Current;
 		temp = TillNull(temp, up);
@@ -359,8 +402,8 @@ public:
 			}
 		}
 	}
-// insert Cell
-	void insertCellByLeftShift(T valCol,T val)
+	// insert Cell
+	void insertCellByLeftShift(T valCol, T val)
 	{
 		Node<T>* temp = this->Current;
 		T tempPre = temp->data;
@@ -408,11 +451,29 @@ public:
 			temp = temp->down;
 		}
 	}
+	// save the thing
+	void save(string filename)
+	{
+		Node<T>* temp = TopLeft;
+		Node<T>* tempRow = TopLeft;
+		filename += ".txt";
+		fstream file;
+		file.open(filename, ios::out);
+		while (tempRow != nullptr)
+		{
+			while (temp != nullptr)
+			{
+				file << temp->data << ",";
+				temp = temp->right;
+			}
+			file << endl;
+			tempRow = tempRow->down;
+			temp = tempRow;
+		}
+		file.close();
+	}
 
-
-
-
-// delete Column
+	// delete Column
 	void deleteColumn()
 	{
 		Node<T>* temp = this->Current;
@@ -474,8 +535,8 @@ public:
 		if (x == down)
 			return temp->down;
 	}
-// Moe cuurrent till null
-	Node<T>* TillNull(Node<T>* temp,positionCell x)
+	// Moe cuurrent till null
+	Node<T>* TillNull(Node<T>* temp, positionCell x)
 	{
 		while (PositionGetter(temp, x) != nullptr)
 		{
@@ -483,7 +544,7 @@ public:
 		}
 		return temp;
 	}
-// Delete By LeftShift
+	// Delete By LeftShift
 	void DeleteCellByLeftShift()
 	{
 		Node<T>* temp = this->Current;
@@ -506,7 +567,7 @@ public:
 			tempNext = tempNext->down;
 		}
 	}
-// Clear whole 
+	// Clear whole 
 	void clearWhole(T val)
 	{
 
@@ -519,7 +580,7 @@ public:
 
 				tempRow->data = val;
 				tempRow = tempRow->right;
-				
+
 			}
 
 			tempCol = tempCol->down;
@@ -533,12 +594,43 @@ public:
 template <typename T>
 class FrontEnd {
 public:
+
+	string fileNamePath = "establishedMongo.txt";
+	int rows_of_screen, columns_of_screen;
+	int ly = 7;
+	int lx = 4;
+	int select = 0;
+	bool mover = false;
+	int rows_to_fit, columns_to_fit;
 	int actRow = 3;
 	int actCol = 3;
-// it print the whole data in tabular form
-    void printGrid(Node<T>* top)
+	vector<string> filenames;
+	int fileNameSingle;
+	// arraryPrintEr
+	void ArrayPrinter(vector<string> data, int x, int y, int size)
 	{
-		
+		if (size < 0)
+		{
+			return;
+		}
+		gotoxy(x, y);
+		cout << "   |~| " << data[size];
+		ArrayPrinter(data, x, y + 4, size - 1);
+	}
+	void ArrayPrinterErasser(int x, int y, int size)
+	{
+		if (size < 0)
+		{
+			return;
+		}
+		gotoxy(x, y);
+		cout << "                             ";
+		ArrayPrinterErasser(x, y + 4, size - 1);
+	}
+	// it print the whole data in tabular form
+	void printGrid(Node<T>* top)
+	{
+
 		Node<T>* tempRow = top;
 		Node<T>* tempCol = top;
 		int x = 3;
@@ -551,21 +643,49 @@ public:
 		{
 			while (tempRow != nullptr)
 			{
-				
-				CellPrint(tempRow,x,y);
+
+				CellPrint(tempRow, x, y);
 				tempRow = tempRow->right;
 				x += 11;
 			}
-			
+
 			tempCol = tempCol->down;
 			tempRow = tempCol;
 			y += 5;
 			x = 3;
 		}
 		y = 3;
-		
+
 	}
-// print row
+	// store File NAme
+	void storeFileName()
+	{
+		fstream file;
+		file.open(fileNamePath, ios::out);
+		for (int i = 0; i < filenames.size(); i++)
+		{
+			file << filenames[i] << endl;
+		}
+		file.close();
+	}
+	// fetch data 
+	void fetchData()
+	{
+		fstream file;
+		file.open(fileNamePath, ios::in);
+		if (file.is_open())
+		{
+			while (!file.eof())
+			{
+				string line = "";
+				getline(file, line);
+				if (line != "")
+					filenames.push_back(line);
+			}
+		}
+		file.close();
+	}
+	// print row
 	void printtb(Node<T>* temp)
 	{
 		Node<T>* H = temp;
@@ -575,11 +695,11 @@ public:
 		char val = 'A';
 		while (temp != nullptr || H != nullptr)
 		{
-			
+
 			if (temp != nullptr)
 			{
 				gotoxy(x, 1);
-					cout <<"\033[31m" << i;
+				cout << "\033[31m" << i;
 				i++;
 				x += 11;
 				temp = temp->right;
@@ -595,24 +715,24 @@ public:
 		}
 
 	}
-// it print the cell on screen 
-	void CellPrint(Node<T>* temp,int x,int y)
+	// it print the cell on screen 
+	void CellPrint(Node<T>* temp, int x, int y)
 	{
-	
-		gotoxy(x,y);
+
+		gotoxy(x, y);
 		cout << "\033[36m" << "----------";
-		gotoxy(x, y+1);
+		gotoxy(x, y + 1);
 		cout << "|        |";
-		gotoxy(x, y+2);
+		gotoxy(x, y + 2);
 		cout << "|        |";
-		gotoxy(x, y+3);
+		gotoxy(x, y + 3);
 		cout << "|        |";
-		gotoxy(x, y+4);
+		gotoxy(x, y + 4);
 		cout << "----------";
-		gotoxy(x+3, y+2);
+		gotoxy(x + 3, y + 2);
 		cout << temp->data;
 	}
-// to point at particular position
+	// to point at particular position
 	void gotoxy(int x, int y)
 	{
 		COORD coordinates;
@@ -620,7 +740,7 @@ public:
 		coordinates.Y = y;
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
 	}
-	Node<T>* assigner(Node<T>* temp,Node<T>* next)
+	Node<T>* assigner(Node<T>* temp, Node<T>* next)
 	{
 		if (next != nullptr)
 		{
@@ -642,10 +762,173 @@ public:
 		gotoxy(x, y + 4);
 		cout << "##########";
 		gotoxy(x + 3, y + 2);
-		cout <<"\033[34m" << temp->data;
+		cout << "\033[34m" << temp->data;
+	}
+	// Begin Logo
+	void logo()
+	{
+		int x, y;
+		screen_size();
+		system("cls");
+		x = columns_to_fit;
+		y = 5;
+		gotoxy(x, y);
+		cout << "\033[36m" << ".88b  d88.d 888888b  d8b" << "\033[32m" << "   db d888888b      d88888b db     db. o88b. d88888b db";
+		gotoxy(x, y + 1);
+		cout << " 88'YbdP`88   `88'   888o  88   `88'        88'     `8b  d8' d8P  Y8 88'     88 ";
+		gotoxy(x, y + 2);
+		cout << " 88  88  88   " << "\033[38m" << " 88    88V8o 88    88         88ooooo" << "\033[31m" << "  `8bd8'  8P      88ooooo 88";
+		gotoxy(x, y + 3);
+		cout << " 88  88  88    88    88 V8o88    88" << "\033[33m" << "         88~~~~~.  dPYb.  8b      88~~~~~ 88";
+		gotoxy(x, y + 4);
+		cout << " 88  88  88" << "\033[36m" << "   .88.   88  V888   .88.       " << "\033[38m" << " 88.     .8P  Y8.Y8b  d8 88.88bo  oo.";
+		gotoxy(x, y + 5);
+		cout << " YP  YP  YP Y888888P VP   V8P " << "\033[32m" << "Y888888P      Y88888P YP    YP" << "\033[31m" << "  `Y88P' Y88888P Y88888P";
+	}
+	int screen_size()
+	{
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+		columns_of_screen = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+		rows_of_screen = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+		rows_to_fit = rows_of_screen / 4;
+		columns_to_fit = columns_of_screen / 4;
+		return 0;
+	}
+
+	void Line()
+	{
+		gotoxy(lx + 3, ly);
+		cout << "|###################|";
+	}
+	void LineEraser()
+	{
+		gotoxy(lx + 3, ly);
+		cout << "                         ";
+
+	}
+	void Selector(int size, int intial)
+	{
+		if (GetAsyncKeyState(VK_UP))
+		{
+			if (ly > intial)
+			{
+				LineEraser();
+				ly -= 4;
+				Line();
+				select--;
+			}
+		}
+		else if (GetAsyncKeyState(VK_DOWN))
+		{
+			if (ly < 20)
+			{
+				select++;
+				LineEraser();
+				ly += 4;
+				Line();
+			}
+
+		}
+	}
+	void fileArt(int x, int y, T data)
+	{
+		gotoxy(x, y);
+		cout << "/ -/|";
+		gotoxy(x, y + 1);
+		cout << "/ _/ |";
+		gotoxy(x, y + 2);
+		cout << "| |/|";
+		gotoxy(x, y + 3);
+		cout << "| ^ | |";
+		gotoxy(x, y + 4);
+		cout << "|_|/";
+		gotoxy(x, y + 5);
+		cout << data;
+	}
+	void LoadFile()
+	{
+		int x = 20;
+		if (filenames.size() > 5)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				fileArt(x, 30, filenames[i]);
+				x = x + 20;
+			}
+		}
+	}
+	void AllFiles()
+	{
+
+		for (int i = 0; i < filenames.size(); i++)
+		{
+			gotoxy(40, 25 + i);
+			cout << i + 1 << filenames[i];
+		}
+
+	}
+	void selectNumber()
+	{
+		gotoxy(50, 25);
+		cout << "SELECT: ";
+		gotoxy(60, 25);
+		cin >> fileNameSingle;
+		fileNameSingle -= 1;
+	}
+	void Selection()
+	{
+		if (select == 1)
+		{
+
+		}
+		else if (select == 2)
+		{
+			AllFiles();
+			selectNumber();
+			mover = true;
+
+		}
+		else if (select == 3)
+		{
+			LoadFile();
+			selectNumber();
+			mover = true;
+		}
+		else if (select == 4)
+		{
+			mover = false;
+		}
+	}
+
+	void FrontPageManage()
+	{
+
+		vector<string> menu = { "New","Load","All Files","Template","Exit" };
+		bool checker = true;
+		bool menuEvent = false;
+		logo();
+		while (checker && mover == false)
+		{
+			if (GetAsyncKeyState(VK_CONTROL))
+			{
+				menuEvent = (!menuEvent);
+				if (menuEvent == true)
+					ArrayPrinter(menu, 4, 5, menu.size() - 1);
+				else
+					ArrayPrinterErasser(4, 5, menu.size() - 1);
+			}
+			if (menuEvent)
+				Selector(menu.size(), 7);
+			if (GetAsyncKeyState(VK_SPACE))
+				break;
+			Sleep(100);
+		}
+		Selection();
 	}
 };
 
+// Functon Module
 template <typename T>
 class FunctionModule {
 public:
@@ -662,7 +945,7 @@ public:
 	bool collector = false;
 	void movementCounter(Node<string>* Cell)
 	{
-		if (GetAsyncKeyState(VK_SHIFT) )
+		if (GetAsyncKeyState(VK_SHIFT))
 		{
 			if (collector == false)
 			{
@@ -679,7 +962,7 @@ public:
 		{
 			movementRight++;
 		}
-		
+
 	}
 	void LeftCall()
 	{
@@ -703,7 +986,7 @@ public:
 		}
 	}
 
-	void Calculator(Node<string>* Cell,FrontEnd<string> x)
+	void Calculator(Node<string>* Cell, FrontEnd<string> x)
 	{
 		Node<string>* temp = Cell;
 		for (int i = 0; i < movementDown; i++)
@@ -742,14 +1025,14 @@ public:
 			subV.push_back(sub);
 			avgV.push_back(sum / movementRight);
 			x.gotoxy(120, 5 + i);
-				cout << i + 1 << ". SUM: " << sum << " DIV: " << div << " MUL: " << mul << " SUB: " << sub << " MAX: " << max <<" MIN: "<<min;
+			cout << i + 1 << ". SUM: " << sum << " DIV: " << div << " MUL: " << mul << " SUB: " << sub << " MAX: " << max << " MIN: " << min;
 			temp = temp->down;
 			Cell = temp;
 		}
 		movementDown = 1;
 		movementRight = 1;
 	}
-	void Paste(Node<string>* Cell, Node<string>* Current,int sit)
+	void Paste(Node<string>* Cell, Node<string>* Current, int sit)
 	{
 		Node<string>* temp = Cell;
 		Node<string>* curr = Current;
@@ -765,13 +1048,13 @@ public:
 			{
 				if (curr == nullptr)
 					break;
-				if(sit == 1 || sit == 2)
-				curr->data = Cell->data;
+				if (sit == 1 || sit == 2)
+					curr->data = Cell->data;
 				if (sit == 2)
 					Cell->data = "0";
 				curr = curr->right;
 				Cell = Cell->right;
-				
+
 			}
 			sumV.push_back(sum);
 			divV.push_back(div);
@@ -793,21 +1076,34 @@ public:
 	}
 };
 
+
+
+void fetcher(string fileName, Node<string>* TopLeft);
+
 void main()
 {
-	Excell<string>* link = new Excell<string>("22");
+start:
 	FrontEnd<string> x;
+	x.fetchData();
+	x.FrontPageManage();
+	Excell<string>* link = new Excell<string>("22");
+	if (x.mover == true)
+	{
+		link = new Excell<string>("22", x.filenames[x.fileNameSingle]);
+		fetcher(x.filenames[x.fileNameSingle], link->TopLeft);
+		cout << "ADAFAF";
+		getchar();
+	}
 	FunctionModule<Node<string>> Functions;
-	x.printGrid(link->TopLeft); 
+	x.printGrid(link->TopLeft);
 	int sit = 0;
-	link->Current->data = "3";
 	while (true)
 	{
-		
-		
+
+
 		if (GetAsyncKeyState(VK_UP))
 		{
-			x.CellPrint(link->Current,x.actCol,x.actRow);
+			x.CellPrint(link->Current, x.actCol, x.actRow);
 			if (link->PositionGetter(link->Current, up) != nullptr)
 			{
 				x.activeCell(link->Current, x.actCol, x.actRow -= 5);
@@ -828,8 +1124,8 @@ void main()
 			}
 			else
 				x.activeCell(link->Current, x.actCol, x.actRow);
-			link->Current = x.assigner(link->Current,link->PositionGetter(link->Current, righ));
-		
+			link->Current = x.assigner(link->Current, link->PositionGetter(link->Current, righ));
+
 		}
 		else if (GetAsyncKeyState(VK_LEFT))
 		{
@@ -841,8 +1137,8 @@ void main()
 			}
 			else
 				x.activeCell(link->Current, x.actCol, x.actRow);
-			link->Current = x.assigner(link->Current,link->PositionGetter(link->Current, lef));
-		
+			link->Current = x.assigner(link->Current, link->PositionGetter(link->Current, lef));
+
 		}
 		else if (GetAsyncKeyState(VK_DOWN))
 		{
@@ -855,15 +1151,15 @@ void main()
 			else
 				x.activeCell(link->Current, x.actCol, x.actRow);
 
-			link->Current = x.assigner(link->Current,link->PositionGetter(link->Current, down));
-			
+			link->Current = x.assigner(link->Current, link->PositionGetter(link->Current, down));
+
 		}
 
 		else if (GetAsyncKeyState(VK_CONTROL))
 		{
 			if (GetAsyncKeyState(0x57))
 			{
-				
+
 				link->addRowAbove("123", link->Current);
 				x.printGrid(link->TopLeft);
 				link->Current = link->TopLeft;
@@ -895,7 +1191,7 @@ void main()
 				x.actCol = 3;
 				x.actRow = 3;
 			}
-			
+
 			if (GetAsyncKeyState(0x42))
 			{
 				sit = 1;
@@ -906,7 +1202,7 @@ void main()
 			}
 			if (GetAsyncKeyState(0x56))
 			{
-				Functions.Paste(Functions.PositionSt, link->Current,sit);
+				Functions.Paste(Functions.PositionSt, link->Current, sit);
 				x.printGrid(link->TopLeft);
 			}
 			if (GetAsyncKeyState(0x49))
@@ -947,11 +1243,11 @@ void main()
 			{
 				link->deleteColumn();
 			}
-			else if(check == 2)
+			else if (check == 2)
 			{
 				link->deleteRow();
 			}
-			
+
 			x.printGrid(link->TopLeft);
 			link->Current = link->TopLeft;
 			x.actCol = 3;
@@ -960,10 +1256,76 @@ void main()
 		else if (GetAsyncKeyState(VK_TAB) && Functions.PositionSt != nullptr)
 		{
 			x.printGrid(link->TopLeft);
-			Functions.Calculator(Functions.PositionSt,x);
+			Functions.Calculator(Functions.PositionSt, x);
 
+
+		}
+		else if (GetAsyncKeyState(VK_F1)) {
+			string name = "new doc" + to_string(x.filenames.size());
+			x.gotoxy(10, 1);
+			cout << "WRITE THE NAME OF FILE: ";
+			x.gotoxy(37, 1);
+			cin >> name;
+			x.filenames.push_back(name);
+			x.storeFileName();
+			link->save(name);
+			break;
+		}
+		else if (GetAsyncKeyState(0x4D))
+		{
+			string num;
+			x.gotoxy(x.actCol + 1, x.actRow + 2);
+			cout << "    ";
+			x.gotoxy(x.actCol + 1, x.actRow + 2);
+			cin >> num;
+			link->Current->data = num;
+			x.printGrid(link->TopLeft);
+		}
+		else if (GetAsyncKeyState(VK_ESCAPE))
+		{
+			string name = "new doc" + to_string(x.filenames.size());
+			x.filenames.push_back(name);
+			x.storeFileName();
+			link->save(name);
+			break;
+		}
+		else if (GetAsyncKeyState(VK_F2))
+		{
+			x.printGrid(link->TopLeft);
+			;
 		}
 		Functions.movementCounter(link->Current);
 		Sleep(100);
 	}
+	goto start;
+}
+
+void fetcher(string fileName, Node<string>* TopLeft)
+{
+	fstream file;
+	file.open(fileName + ".txt", ios::in);
+	string line;
+	Node<string>* temp = TopLeft;
+	Node<string>* tempRow = TopLeft;
+
+	while (tempRow != nullptr)
+	{
+		int i = 0;
+		getline(file, line);
+		while (temp != nullptr)
+		{
+			string text = "";
+			while (line[i] != ',')
+			{
+				text += line[i];
+				i++;
+			}
+			i++;
+			temp->data = text;
+			temp = temp->right;
+		}
+		tempRow = tempRow->down;
+		temp = tempRow;
+	}
+	file.close();
 }
